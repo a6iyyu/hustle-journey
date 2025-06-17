890b194e5a2f2866eaf31cbe1d0ff42013928b61
VozSoldat <muhamaderril7@gmail.com>
2025-06-17 16:55:23 +0700

    feat(UI): add Narration-Action System
    Narration-Action System has two main elements: a narration section and a section of list of action choices. These then wrapped inside a prefab for automated creation and destruction through script.

--- Assets/App/Scripts/Controllers/Testing1.cs
+++ Assets/App/Scripts/Controllers/Testing1.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 using System.Reflection;
 using System.Text;
 using Assets.App.Scripts.Singletons;
+using App.UIElements.NarrativeSection.DTOs;
+using System.Collections.Generic;
 
 public class Testing1 : MonoBehaviour
 {
     void Start()
     {
-        Test();
+        // Test();
+        Test2();
     }
 
     public void Test()
@@ -28,25 +31,64 @@ public class Testing1 : MonoBehaviour
         var petFromDict = PetModel.Find(myPet.Id);
         Debug.Log(VarDump(myPet));
         Debug.Log(VarDump(petFromDict));
-        
+
     }
-        /// <summary>
-        /// Dumps the given object's properties and values to a string.
-        /// </summary>
-        /// <param name="obj">Object to dump.</param>
-        /// <returns>A string containing the object's properties and values.</returns>
-        /// <remarks>
-        /// This function is useful for debugging, since it shows the object's properties and values
-        /// in a readable format.
-        /// <para>
-        /// It uses reflection to get the object's properties, and <see cref="StringBuilder"/> to
-        /// build the string.
-        /// </para>
-        /// <para>
-        /// It also handles exceptions that may occur while getting a property's value, and
-        /// shows the exception message instead of the value.
-        /// </para>
-        /// </remarks>
+    public void Test2()
+    {
+        var sections = new List<NarrativeSectionData>
+        {
+            new NarrativeSectionData
+            {
+                Text = "Gang Mejayan > Rumahku\nIni adalah kamar kecil yang nyaman...",
+                Actions = new List<ActionChoice>()
+            },
+            new NarrativeSectionData
+            {
+                Text = "Rena sedang duduk di kasur, menyisir rambutnya.",
+                Actions = new List<ActionChoice>
+                {
+                    new ActionChoice { Label = "Ajak bicara Rena", OnClick = () => Debug.Log("Ngobrol dengan Rena...") },
+                    new ActionChoice { Label = "Tanyakan kabar", OnClick = () => Debug.Log("Kamu bertanya kabar...") }
+                }
+            },
+            new NarrativeSectionData
+            {
+                Text = "Kulkas berdengung pelan. Sedikit berdebu.",
+                Actions = new List<ActionChoice>
+                {
+                    new ActionChoice { Label = "Ambil kue", OnClick = () => Debug.Log("Kamu ambil kue.") },
+                    new ActionChoice { Label = "Bersihkan kulkas", OnClick = () => Debug.Log("Membersihkan...") }
+                }
+            },
+            new NarrativeSectionData
+            {
+                Text = "Pintu kamar terbuka menuju ruang tengah.",
+                Actions = new List<ActionChoice>
+                {
+                    new ActionChoice { Label = "Keluar kamar", OnClick = () => Debug.Log("Keluar ke ruang tengah.") }
+                }
+            }
+        };
+        // FindObjectOfType<NarrativeRenderer>().Render(sections);
+        FindFirstObjectByType<NarrativeRenderer>().Render(sections);
+    }
+    /// <summary>
+    /// Dumps the given object's properties and values to a string.
+    /// </summary>
+    /// <param name="obj">Object to dump.</param>
+    /// <returns>A string containing the object's properties and values.</returns>
+    /// <remarks>
+    /// This function is useful for debugging, since it shows the object's properties and values
+    /// in a readable format.
+    /// <para>
+    /// It uses reflection to get the object's properties, and <see cref="StringBuilder"/> to
+    /// build the string.
+    /// </para>
+    /// <para>
+    /// It also handles exceptions that may occur while getting a property's value, and
+    /// shows the exception message instead of the value.
+    /// </para>
+    /// </remarks>
     public static string VarDump(object obj)
     {
         var result = new StringBuilder();
---
+++ Assets/App/UIElements/NarrativeSection/Scripts/NarrativeSectionUI.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using App.UIElements.NarrativeSection.DTOs;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NarrativeSectionUI : MonoBehaviour
+{
+    public TextMeshProUGUI narrativeText;
+    public Transform actionContainer;
+    public GameObject actionButtonPrefab;
+
+    public void Set(string text, List<ActionChoice> actions)
+    {
+        narrativeText.text = text;
+
+        // Clear old buttons
+        foreach (Transform child in actionContainer)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // Add buttons
+        foreach (var action in actions)
+        {
+            var go = Instantiate(actionButtonPrefab, actionContainer);
+            go.GetComponentInChildren<TextMeshProUGUI>().text = action.Label;
+            go.GetComponent<Button>().onClick.AddListener(() => action.OnClick.Invoke());
+        }
+    }
+}
