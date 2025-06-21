eca056239d73c210ca2617b277fea5b4cb79a402
VozSoldat <muhamaderril7@gmail.com>
2025-06-21 10:51:06 +0700

    Update Needs.cs

--- Assets/App/Scripts/Player/Needs.cs
+++ Assets/App/Scripts/Player/Needs.cs
@@ -1,73 +1,73 @@
 public class Needs
 {
-    private int _satiety;
-    private int _energy;
-    private int _recreation;
-    private int _hygiene;
-    private int _bladder;
-    private int _social;
-    private int _health;
-    private int _pain;
-    private int _drunkenness;
-    private int _discontent;
+    private float _satiety;
+    private float _energy;
+    private float _recreation;
+    private float _hygiene;
+    private float _bladder;
+    private float _social;
+    private float _health;
+    private float _pain;
+    private float _drunkenness;
+    private float _discontent;
 
     // main needs
-    public int Satiety
+    public float Satiety
     {
         get => _satiety;
         set => _satiety = Clamp(value);
     }
-    public int Energy
+    public float Energy
     {
         get => _energy;
         set => _energy = Clamp(value);
     }
-    public int Recreation
+    public float Recreation
     {
         get => _recreation;
         set => _recreation = Clamp(value);
     }
-    public int Hygiene
+    public float Hygiene
     {
         get => _hygiene;
         set => _hygiene = Clamp(value);
     }
-    public int Bladder
+    public float Bladder
     {
         get => _bladder;
         set => _bladder = Clamp(value);
     }
 
     // secondary needs
-    public int Social
+    public float Social
     {
         get => _social;
         set => _social = Clamp(value);
     }
-    public int Health
+    public float Health
     {
         get => _health;
         set => _health = Clamp(value);
     }
 
     // negative effects
-    public int Pain
+    public float Pain
     {
         get => _pain;
         set => _pain = Clamp(value);
     }
-    public int Drunkenness
+    public float Drunkenness
     {
         get => _drunkenness;
         set => _drunkenness = Clamp(value);
     }
-    public int Discontent
+    public float Discontent
     {
         get => _discontent;
         set => _discontent = Clamp(value);
     }
 
-    private int Clamp(int value)
+    private float Clamp(float value)
     {
         if (value < 0) return 0;
         if (value > 20) return 20;
